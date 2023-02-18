c7e86044252cbac7b87e2723d9ca867230194b5b
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-18 18:00:28 +0200

    Add demo tests for single and multiple checkbox in Selenium Easy

---
+++ SeleniumFramework1/Pages/SeleniumEasy/CheckBoxDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework1.Pages.SeleniumEasy
+{
+    public class CheckBoxDemo
+    {
+        private static string inputSingleCheckbox = "//*[@id='isAgeSelected']";
+        private static string fieldSingleCheckboxSuccessMessage = "//*[@id='txtAge']";
+        private static string inputsMultipleCheckbox = "//*[@class='cb1-element']";
+        private static string buttonMultipleCheckboxes = "//*[@id='check1']";
+
+        public static void Open()
+        {
+            Driver.OpenUrl("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        }
+
+        public static void ClickSingleCheckbox()
+        {
+            Common.ClickElement(inputSingleCheckbox);
+        }
+
+        public static string GetSingleCheckboxSuccessMessage()
+        {
+            return Common.GetElementText(fieldSingleCheckboxSuccessMessage);
+        }
+
+        public static void ClickEachOfMultipleCheckboxes()
+        {
+            Common.ClickElements(inputsMultipleCheckbox);
+        }
+
+        public static string GetMultipleCheckboxButtonText()
+        {
+            return Common.GetAttributeValue(buttonMultipleCheckboxes, "value");
+        }
+    }
+}
---
+++ SeleniumTests1/SeleniumEasy/CheckBoxDemoTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using SeleniumFramework1.Pages.SeleniumEasy;
+using SeleniumFramework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests1.SeleniumEasy
+{
+    internal class CheckBoxDemoTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+            CheckBoxDemo.Open();
+        }
+
+        [Test]
+        public void SingleCheckbox()
+        {
+            string expectedMessage = "Success - Check box is checked";
+
+            CheckBoxDemo.ClickSingleCheckbox();
+            string actualResult = CheckBoxDemo.GetSingleCheckboxSuccessMessage();
+
+            Assert.AreEqual(expectedMessage, actualResult);
+        }
+
+        [Test]
+        public void MultipleCheckboxes()
+        {
+            string expectedButtonText = "Uncheck All";
+
+            CheckBoxDemo.ClickEachOfMultipleCheckboxes();
+            string actualButtonText = CheckBoxDemo.GetMultipleCheckboxButtonText();
+
+            Assert.AreEqual(expectedButtonText, actualButtonText);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
