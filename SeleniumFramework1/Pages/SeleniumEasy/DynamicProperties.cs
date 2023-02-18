553680e3036bad2b596d0e476f880c6a385365e5
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-18 18:52:31 +0200

    Add demo for Waits in DemoQA

---
+++ SeleniumFramework1/Pages/SeleniumEasy/DynamicProperties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework1.Pages.SeleniumEasy
+{
+    public class DynamicProperties
+    {  // DemoQA puslapis
+
+        public static bool WaitForButtonToBeClickable()
+        {
+            string locator = "//*[@id='enableAfter']";
+            Common.WaitForElementToBeClickable(locator);
+            return Common.CheckIfElementIsEnabled(locator);
+        }
+
+        public static void Open()
+        {
+            Driver.OpenUrl("https://demoqa.com/dynamic-properties");
+        }
+
+        public static void WaitForButtonClassToChange()
+        {
+            string locator = "//*[@id='colorChange']";
+            Common.WaitForElementAttributeToContainValue(locator, "class", "text-danger");
+        }
+
+        public static void WaitForButtonTextToBeRed()
+        {
+            string locator = "//*[@id='colorChange']";
+            Common.WaitForElementCssPropertyToBe(locator, "color", "rgba(255, 0, 0, 1)");
+        }
+
+        public static void WaitForButtonToBeVisible()
+        {
+            string locator = "//*[@id='visibleAfter']";
+            Common.WaitForElementToBeVisible(locator);
+        }
+    }  
+}
---
+++ SeleniumTests1/SeleniumEasy/DynamicPropertiesTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using SeleniumFramework1.Pages.SeleniumEasy;
+using SeleniumFramework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests1.SeleniumEasy
+{
+    internal class DynamicPropertiesTests
+    { // DemoQA puslapis
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+            DynamicProperties.Open();
+        }
+
+        [Test]
+        public void WaitForButtonToBeClickable()
+        {
+            Assert.IsTrue(DynamicProperties.WaitForButtonToBeClickable());
+        }
+
+        [Test]
+        public void WaitForButtonClassToChange()
+        {
+            DynamicProperties.WaitForButtonClassToChange();
+        }
+
+        [Test]
+        public void WaitForButtonBorderToBeRed()
+        {
+            DynamicProperties.WaitForButtonTextToBeRed();
+        }
+
+        [Test]
+        public void WaitForButtonToBeVisible()
+        {
+            DynamicProperties.WaitForButtonToBeVisible();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
