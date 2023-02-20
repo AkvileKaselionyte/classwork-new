06f4811eefee922dbd30d86e2a9b77e7007740b4
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-20 20:21:01 +0200

    Add fourth task solution

---
+++ SeleniumFramework1/Pages/SeleniumEasy/DynamicDataLoading.cs
@@ -0,0 +1,59 @@
+﻿using SeleniumFramework1.Pages.DemoQA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework1.Pages.SeleniumEasy
+{
+    public class DynamicDataLoading
+    {
+        private static string buttonGetNewUser = "//*[@id='save']";
+        private static string image = "//*[@id='loading']/img";
+
+
+        public static void Open()
+        {
+            Driver.OpenUrl("https://demo.seleniumeasy.com/dynamic-data-loading-demo.html");
+        }
+
+        public static void ClickButtonGetNewUser()
+        {
+            Common.ClickElement(buttonGetNewUser);
+        }
+
+        public static void WaitForImageToBeVisible()
+        {
+            Common.WaitForElementToBeVisible(image);
+        }
+
+        public static bool GetFirstNameMessage()
+        {
+            string attributeText = Common.GetAttributeValue(image, "text");
+
+            if (attributeText.Contains("First Name :"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool GetLastNameMessage()
+        {
+            string attributeText = Common.GetAttributeValue(image, "text");
+
+            if (attributeText.Contains("Last Name :"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SeleniumTests1/SeleniumEasy/DynamicDataLoadingTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SeleniumFramework1.Pages.SeleniumEasy;
+using SeleniumFramework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumFramework1.Pages.DemoQA;
+
+namespace SeleniumTests1.SeleniumEasy
+{
+    internal class DynamicDataLoadingTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+            DynamicDataLoading.Open();
+        }
+
+        [Test]
+        public void LoadingTheDataDynamically()
+        {
+            DynamicDataLoading.ClickButtonGetNewUser();
+            DynamicDataLoading.WaitForImageToBeVisible();
+            Assert.IsTrue(DynamicDataLoading.GetFirstNameMessage());
+            Assert.IsTrue(DynamicDataLoading.GetLastNameMessage());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
