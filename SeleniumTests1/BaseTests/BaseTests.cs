a102360d180921c739014b85584ea62680e5203d
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-22 21:05:01 +0200

    Add class BaseTests

---
+++ SeleniumTests1/BaseTests/BaseTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using SeleniumFramework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests1.BaseTests
+{
+    internal class BaseTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+
+    }
+}
